d4558452cfbed63e0fcd02141d642efffe8ba173
farhadi <farhadi@gmail.com>
2019-08-24 19:41:26 +0430

    support multithreading and command argument parameters fix bugs create test folder

--- getdatafromsite.py
+++ getdatafromsite.py
@@ -4,149 +4,173 @@ except ImportError:
     import urllib
 from io import BytesIO 
 import requests 
+import threading
 import codecs
 import os
 import psutil
 import jalali
 import time
+import sys,getopt
 from findnumber import findnumber
 import json as m_json
 from datetime import date,timedelta
 from PIL import Image
 from PIL import ImageFilter
 from selenium import webdriver
-#rom HTMLParser import HTMLParser
 from selenium.webdriver.firefox.options import Options
 
-data_path='data/';
-directory = os.path.dirname(data_path)
-try:
-    os.stat(directory)
-except:
-    os.mkdir(directory)
-walk_days=0;
-max_walk_days=365;
-options = Options()
-options.add_argument('--headless')
 
-
-drivermain = webdriver.Firefox(options=options)
-drivermain.get('http://www.rrk.ir/News/NewsList.aspx')
-number_reader=findnumber();
-first_code_number=0
-while (walk_days<max_walk_days):
-    today=date.today()-timedelta(days=walk_days)
-    today_shamsi=jalali.Gregorian(today).persian_string("{}{:02d}{:02d}")
-    print(today_shamsi)
-    
-    #alert=drivermain.switch_to.alert
-    #alert.accept()
-    #time.sleep(5)
-    
-    NewsDate_input=drivermain.find_element_by_id('cphMain_dteFromNewspaperDate_dteFromNewspaperDate_txtDate')
-    NewsDate_input.send_keys(today_shamsi)
-    #time.sleep(5)
+def getdatafromsearchresult(search):
+    global first_code_number
+    global number_reader
+    search_result_link=search.find_element_by_tag_name('a').get_attribute('href')
+    print(search_result_link)
+    search_res_code=search_result_link.split('=')
+    if first_code_number==0 :
+        first_code_number = search_res_code[1]
+    else:
+        if first_code_number==search_res_code[1]:
+            return
+    image_name=data_path+search_res_code[1]+'.jpg'
+    result_datafile=data_path+'res_'+search_res_code[1]+'.txt'
+    if os.path.isfile(result_datafile):
+        print(result_datafile)
+        return
+    driver = webdriver.Firefox(options=options)
+    driver.get(search_result_link)
+    imglinks=driver.find_elements_by_id('imgCaptcha')
+                
+    for img_link in imglinks:
+        imglink=img_link.get_attribute('src')
+                
+    image=Image.open(BytesIO(requests.get(imglink).content))
+    img=image.convert('L',dither=Image.NONE)
+    img=img.filter(ImageFilter.GaussianBlur())
+    img=img.filter(ImageFilter.SHARPEN)
+    if image_view:
+        img.show()
+    image.close()
+    ###################################
+    input_code=number_reader.get_numberstr_from_image(img)
+    print(input_code)
+    ###################################
+    img.close()
+                
+    input_box = driver.find_element_by_id('txtCaptcha')
+    input_box.send_keys(input_code)
+                
+    submit_btn=driver.find_element_by_class_name('btnSearch')
+    submit_btn.click()
+                
     try:
-        btn_search = drivermain.find_element_by_id('cphMain_btnSearch')
-        btn_search.click()
-        #time.sleep(1)
-        image_name=''
-        #alert=drivermain.switch_to.alert
-        #alert.accept()
-        print ('***search result loaded***')
-        page_no=1
-
-        while True:
-            search_results1=drivermain.find_elements_by_class_name('ShowNBut')
-            #print(search_results1)
-            if len(search_results1)==0:
-                break
-            for search in search_results1:
-                search_result_link=search.find_element_by_tag_name('a').get_attribute('href')
-                print(search_result_link)
-                search_res_code=search_result_link.split('=')
-                if first_code_number==0 :
-                    first_code_number = search_res_code[1]
-                else:
-                    if first_code_number==search_res_code[1]:
-                        break;
-                image_name=data_path+search_res_code[1]+'.jpg'
-                result_datafile=data_path+'res_'+search_res_code[1]+'.txt'
-                if os.path.isfile(result_datafile):
-                    print(result_datafile)
-                    continue
-                #print search_res_code[1]
-                driver = webdriver.Firefox(options=options)
-                driver.get(search_result_link)
-                imglinks=driver.find_elements_by_id('imgCaptcha')
-                
-                for img_link in imglinks:
-                    imglink=img_link.get_attribute('src')
-        
-                #try:
-                #    urllib.request.urlretrieve(imglink,'image_name.jpg')
-                #except Exception:
-                #    urllib.urlretrieve(imglink,'image_name.jpg')
-                #
-                #image=Image.open('image_name.jpg')
-                
-                image=Image.open(BytesIO(requests.get(imglink).content))
-                #image=Image.open(urllib2.urlopen(imglink))
-                #img=image.filter(ImageFilter.BLUR)
-                img=image.convert('L',dither=Image.NONE)
-                img=img.filter(ImageFilter.GaussianBlur())
-                #img=img.filter(ImageFilter.BLUR)
-                img=img.filter(ImageFilter.SHARPEN)
-                #img=img.filter(ImageFilter.SMOOTH_MORE)
-                img.show()
-                image.close()
-                #img.save(image_name)
-                ###################################
-                input_code=number_reader.get_numberstr_from_image(img)
-                print(input_code)
-                ###################################
-                img.close()
-                #input_code=input('Enter numbercode :')
-                
-                input_box = driver.find_element_by_id('txtCaptcha')
-                input_box.send_keys(input_code)
-                
-                submit_btn=driver.find_element_by_class_name('btnSearch')
-                submit_btn.click()
-                
-                #detail_rcv=driver.find_element_by_class_name('Padder2')
-                try:
-                    detail_rcv=driver.find_element_by_id('cphMain_pnlNewsInfo')
-                    strlink=detail_rcv.text
-                    save_output = codecs.open(result_datafile,'w','utf-8')
-                    save_output.write(strlink)
-                    save_output.close()
-                except Exception as e:
-                    print('Exceptions....: %s'% e)
-                # print search.text
-                # print '-----------'
-                for proc in psutil.process_iter():
-                    if proc.name() == "display":
-                        proc.kill()
-                driver.close()
-            btn_next = drivermain.find_element_by_id('cphMain_rptPagingRec_btnNextPage')
-            btn_next.click()
-            print('click Next Page %d'%( page_no))
-            page_no +=1
-            if  page_no > 10 :
-                break;
-
+        detail_rcv=driver.find_element_by_id('cphMain_pnlNewsInfo')
+        strlink=detail_rcv.text
+        save_output = codecs.open(result_datafile,'w','utf-8')
+        save_output.write(strlink)
+        save_output.close()
     except Exception as e:
-        print("Except:")
-        print(u'->'.join(e).encode('utf-8'))
-        drivermain = webdriver.Firefox(options=options)
-        drivermain.get('http://www.rrk.ir/News/NewsList.aspx')        
+        print('Exceptions....: %s'% e)
+    # print search.text
+    # print '-----------'
+    if image_view:
+        for proc in psutil.process_iter():
+            if proc.name() == "display":
+                proc.kill()
+    driver.close()
 
-    walk_days+=1;
-    first_code_number=0
-    drivermain.close()
+def main(argv):
+    mthreading=False
+    image_view=False
+    opts=[]
+    try:
+        opts,args = getopt.getopt(argv,"hst",["showimage","threading",])
+    except getopt.GetoptError as err:
+        print ('Error in argument',str(err))
+    for opt,arg in opts:
+        if opt=='-h':
+            print('getdatafromsite.py --showimage --threading')
+            print('parameters are disabled by default ')
+            sys.exit()
+        elif opt=='--showimage':
+            image_view=True;
+        elif opt=='--threading':
+            mthreading=True;
+    return image_view,mthreading
+        
+
+if __name__ == "__main__":
+    data_path='data/';
+    walk_days=0;
+    max_walk_days=3*365;
+    options = Options()
+    options.add_argument('--headless')
+    global number_reader
+    global first_code_number
+    global image_view,mthreading
+    image_view=False;
+    mthreading=False;
+    #print(sys.argv)
+    image_view , mthreading = main(sys.argv[1:])
     drivermain = webdriver.Firefox(options=options)
     drivermain.get('http://www.rrk.ir/News/NewsList.aspx')
-
-drivermain.close()
+    number_reader=findnumber();
+    first_code_number=0
+    while (walk_days<max_walk_days):
+        today=date.today()-timedelta(days=walk_days)
+        today_shamsi=jalali.Gregorian(today).persian_string("{}{:02d}{:02d}")
+        print(today_shamsi)
+        
+        #alert=drivermain.switch_to.alert
+        #alert.accept()
+        #time.sleep(5)
+        
+        NewsDate_input=drivermain.find_element_by_id('cphMain_dteFromNewspaperDate_dteFromNewspaperDate_txtDate')
+        NewsDate_input.send_keys(today_shamsi)
+        #time.sleep(5)
+        try:
+            btn_search = drivermain.find_element_by_id('cphMain_btnSearch')
+            btn_search.click()
+            #time.sleep(1)
+            image_name=''
+            #alert=drivermain.switch_to.alert
+            #alert.accept()
+            print ('***search result loaded***')
+            page_no=1
+    
+            while True:
+                search_results1=drivermain.find_elements_by_class_name('ShowNBut')
+                #print(search_results1)
+                if len(search_results1)==0:
+                    break
+                th = None
+                for search in search_results1:
+                    if mthreading :
+                        th=threading.Thread(target=getdatafromsearchresult,args=(search,),name='th_search')
+                        th.start()
+                    else:
+                        getdatafromsearchresult(search)
+                if mthreading:
+                    th.join()
+                btn_next = drivermain.find_element_by_id('cphMain_rptPagingRec_btnNextPage')
+                btn_next.click()
+                page_no +=1
+                if  page_no > 10 :
+                    break;
+                else:
+                    print('click Next Page %d'%( page_no))
+    
+        except Exception as e:
+            print("Except:")
+            print(u'->'.join(str(v) for v in e).encode('utf-8'))
+            drivermain.close()
+            drivermain = webdriver.Firefox(options=options)
+            drivermain.get('http://www.rrk.ir/News/NewsList.aspx')        
+    
+        walk_days+=1;
+        first_code_number=0
+        drivermain.close()
+        drivermain = webdriver.Firefox(options=options)
+        drivermain.get('http://www.rrk.ir/News/NewsList.aspx')
+    
+    drivermain.close()
 
---
+++ test/testkmeans.py
@@ -0,0 +1,138 @@
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans  
+from sklearn import datasets , svm , metrics
+from PIL import Image , ImageDraw , ImageFilter
+import numpy as np
+
+image=Image.open('test.jpg')
+#image=image.rotate(180)
+crope=(30,10,155,80)
+image=image.crop(crope)
+image=image.filter(ImageFilter.GaussianBlur())
+
+img_arr=np.asarray(image,dtype='int32')
+cnt=0
+Data= {'x':[],'y':[]}
+for y in range(len(img_arr)):
+    for x in range(len(img_arr[0])):
+        if img_arr[y][x]<=220:
+            Data['x'].append(x)
+            Data['y'].append(y)
+            cnt+=1
+
+df = DataFrame(Data,columns=['x','y'])
+cluster=4
+cluster2=4
+var=1
+kmeans = KMeans(n_clusters=cluster).fit(df)
+centroids=kmeans.cluster_centers_
+while var==1:
+    for x1 in range(0,cluster):
+        for x2 in range(x1+1,cluster):
+            if (abs(centroids[x1][0]-centroids[x2][0])<=15):
+                cluster2=3
+                break
+    if cluster2==cluster:
+        break
+    else:
+        cluster=cluster2
+        kmeans = KMeans(n_clusters=cluster2).fit(df)
+        centroids=kmeans.cluster_centers_
+        break
+
+#print(len(centroids),centroids[2][0],centroids[2][1])
+
+centroids=sorted(centroids,key = lambda x: x[0])
+print(centroids)
+
+imageofnumber=[]
+for point in centroids:
+    imagin=image.crop((point[0]-15,point[1]-20,point[0]+15,point[1]+23))
+    imageofnumber.append(imagin)
+    imagin.show()
+
+#digits = datasets.load_digits()
+#print(digits.target[0])
+
+
+#images_and_labels = list(zip(digits.images, digits.target))
+#print(len(images_and_labels),images_and_labels[5])
+#for index, (image2, label) in enumerate(images_and_labels[:4]):
+#    plt.subplot(2, 4, index + 1)
+#    plt.axis('off')
+#    plt.imshow(image2, cmap=plt.cm.gray_r, interpolation='nearest')
+#    plt.title('Training: %i' % label)
+
+
+#n_samples = len(digits.images)
+
+#print(n_samples)
+#data = digits.images.reshape((n_samples, -1))
+################custom train
+dataimage=[]
+target=[]
+for j in range(10):
+    for i in range(1,10):
+        try:
+            img=Image.open('train/'+str(i)+str(j)+'.jpg')
+            data_img=np.asarray(img,dtype="int32")
+            data_img=np.concatenate(data_img,axis=0)
+            dataimage.append(data_img)
+            target.append(i)
+        except IOError:
+            pass 
+############################
+print(type(dataimage))
+# Create a classifier: a support vector classifier
+classifier = svm.SVC(gamma=0.000001 , C=100.)
+
+# We learn the digits on the first half of the digits
+#classifier.fit(data[:n_samples // 2], digits.target[:n_samples // 2])
+classifier.fit(dataimage, target)
+
+# Now predict the value of the digit on the second half:
+#expected = digits.target[n_samples // 2:]
+print("---------------")
+print(len(dataimage[0]))
+print("---------------")
+
+dataimage=[]
+for im in imageofnumber:
+    #im.show()
+    data_image=np.asarray( im,dtype="int32")
+    data_image=np.concatenate(data_image,axis=0)
+    dataimage.append(data_image)
+    #dataimage['target']+=[1];
+#dfimg = DataFrame(dataimage,columns=['images','target'])
+#dataofimage=dfimg.images
+#dataofimage=np.concatenate(dataofimage)
+#dataofimage
+print("-->",len(dataimage))
+print("---------------")
+predicted = classifier.predict(dataimage) #data[n_samples // 2:])
+
+#print("Classification report for classifier %s:\n%s\n"
+#      % (classifier, metrics.classification_report(expected, predicted)))
+#print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
+print("predict type: ",predicted)
+numbers=''
+for i in predicted:
+    numbers+=str(i)
+print(numbers)
+#images_and_predictions = list(zip(digits.images[n_samples // 2:], predicted))
+#for index, (image2, prediction) in enumerate(images_and_predictions[:4]):
+#    plt.subplot(2, 4, index + 5)
+#    plt.axis('off')
+#    plt.imshow(image2, cmap=plt.cm.gray_r, interpolation='nearest')
+#    plt.title('Prediction: %i' % prediction)
+
+#plt.show()
+
+
+imagefinal=ImageDraw.Draw(image)
+for point in centroids:
+    imagefinal.rectangle(((point[0]-15,point[1]-20),(point[0]+15,point[1]+23)),outline="black")
+image.show()
+
+
